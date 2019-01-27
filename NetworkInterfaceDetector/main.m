f593bae0a7afa83c347a65941851791859d68398
Thomas Westfeld <westfeld@mac.com>
2019-01-27 08:42:45 +0100

    Initial Commit

---
+++ NetworkInterfaceDetector/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NetworkInterfaceDetector
+//
+//  Created by Thomas Westfeld on 27.01.19.
+//  Copyright © 2019 Thomas Westfeld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
