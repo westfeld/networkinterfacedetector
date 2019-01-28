a04dc8df3473ac87e13bbeef7370dfbba7a7ec7b
Thomas Westfeld <westfeld@mac.com>
2019-01-28 21:07:56 +0100

    Finished example

--- NetworkInterfaceDetector/main.m
+++ NetworkInterfaceDetector/main.m
@@ -7,11 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <SystemConfiguration/SystemConfiguration.h>
+
+/* Callback used if a configuration change on monitored keys was detected.
+ */
+void dynamicStoreCallback(SCDynamicStoreRef store, CFArrayRef changedKeys, void* __nullable info) {
+    CFIndex count = CFArrayGetCount(changedKeys);
+    for (CFIndex i=0; i<count; i++) {
+        NSLog(@"Key \"%@\" was changed", CFArrayGetValueAtIndex(changedKeys, i));
+    }
+}
 
 int main(int argc, const char * argv[]) {
+    NSArray *SCMonitoringInterfaceKeys = @[@"State:/Network/Interface.*"];
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        SCDynamicStoreRef dsr = SCDynamicStoreCreate(NULL, CFSTR("network_interface_detector"), &dynamicStoreCallback, NULL);
+        SCDynamicStoreSetNotificationKeys(dsr, CFBridgingRetain(SCMonitoringInterfaceKeys), NULL);
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), SCDynamicStoreCreateRunLoopSource(NULL, dsr, 0), kCFRunLoopDefaultMode);
+        NSLog(@"Starting RunLoop...");
+        while([[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]]);
     }
     return 0;
 }
